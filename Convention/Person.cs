5d7529b74acbfd654084cb4c68c4f8d02816e334
Daniel Park <intheimageofyeshua@gmail.com>
2024-09-25 13:57:32 +0900

    Added Convention Syntax

---
+++ Convention/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Convention
+{
+    public class Person
+    {
+       public string Name { get; set; } 
+       public DateTime Birthday { get; set; }
+       public string PhoneNumber { get; set; }
+    }
+}
---
+++ Convention/Program.cs
@@ -0,0 +1,38 @@
+﻿using Convention;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    // variable initialization
+    var age = 48;
+    var langs = new string[] { "C#", "Ruby", "C++", "Javascript", };
+    var numbs = new List<int> { 10, 20, 30, 40, };
+
+    var dict = new Dictionary<string, string>
+    {
+      { "ko", "한국어"},
+      { "en", "영어"},
+      { "es", "스페인어"},
+      { "de", "독일어"},
+    };
+
+    var dict2 = new Dictionary<string, string>
+    {
+      ["ko"] = "한국어",
+      ["en"] = "영어",
+      ["es"] = "스페인어",
+      ["de"] = "독일어",
+    };
+
+    // object initialization
+    var person = new Person
+    {
+      Name = "홍길동",
+      Birthday = new DateTime(1995, 11, 24),
+      PhoneNumber = "012-345-6789",
+    };
+
+    Console.WriteLine("{0} : {1} : {2}", person.Name, person.Birthday, person.PhoneNumber);
+  }
+}
