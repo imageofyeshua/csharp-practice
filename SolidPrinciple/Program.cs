2e906ff45a712d61fa029974fa82506d7f554b6b
Daniel Park <intheimageofyeshua@gmail.com>
2024-09-30 23:01:47 +0900

    Created SOLID Principle Project

---
+++ SolidPrinciple/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var names = new Names();
+        var path = names.BuildFilePath();
+
+        if (File.Exists(path))
+        {
+            Console.WriteLine("Names file already exists. Loading names.");
+            names.ReadFromTextFile();
+        }
+        else
+        {
+            Console.WriteLine("Names file does not exists yet.");
+
+            names.AddName("John");
+            names.AddName("not a valid name");
+            names.AddName("Clair");
+            names.AddName("123 definitely not a valid name");
+
+            Console.WriteLine("Saving names to a file");
+            names.WriteToTextFile();
+        }
+
+        Console.WriteLine(names.Format());
+        // Console.ReadKey();
+    }
+}
