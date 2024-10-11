021f06abfe148f3e6f887aa182dcf4500790cb4a
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-11 10:45:27 +0900

    Finished Dice Roll Game

---
+++ DiceRollGame/Game/GuessingGame.cs
@@ -0,0 +1,42 @@
+using DiceRollGame.Game.UserCommunication;
+
+namespace DiceRollGame.Game
+{
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number:");
+                if (guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+                Console.WriteLine("Wrong number.");
+                --triesLeft;
+            }
+            return GameResult.Lose;
+        }
+
+        public static void PrintResult(GameResult gameResult)
+        {
+            string message = gameResult == GameResult.Victory
+                ? "You won :)"
+                : "You lost :(";
+            Console.WriteLine(message);
+        }
+    }
+}
